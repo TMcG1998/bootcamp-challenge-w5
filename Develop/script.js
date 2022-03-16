c1d6fe5f8b0a5c5cebbcbb89d7155fcb65b67528
Tommy McG <thomasjmcgovern98@gmail.com>
2022-03-16 16:47:13 -0400

    more basic tweaking and layout experiments

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,32 @@
 // Use moment.js to get the current date, format it correctly, then display it on load
-var currentDay = moment().format("dddd, MMMM Do");
-var currentDayEl = $("#currentDay").text(currentDay);
+var currentDay = moment();
+var currentDayEl = $("#currentDay").text(currentDay.format("dddd, MMMM Do"));
+
+var times = [];
 
 var blockContainerEl = $("#block-container");
 
+$(".time-block").on("click", "p", function() {
+    var text = $(this)
+    .text()
+    .trim();
+    var textInput = $("<textarea>")
+    .addClass("form-control")
+    .val(text);
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+});
 
-var row = $("<div>").addClass("row");
-var desc = $("<p>").addClass("description").text("Description");
+$(".time-block").on("blur", "textarea", function() {
+    var text = $(this)
+    .val()
+    .trim();
+    var blockP = $("<p>")
+    .addClass("description")
+    .text(text);
+    $(this).replaceWith(blockP);
+})
 
-
-//blockContainerEl.append(row);
+var time = moment().hour(9).minute(0).second(0); //sets time to 9AM
+console.log(time);
+console.log(currentDay);
